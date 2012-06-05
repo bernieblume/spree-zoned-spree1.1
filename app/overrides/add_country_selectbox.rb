a98ede7ba4dba5be7bce401415d860fdad345748
Bernd Blume <bernd.blume@railando.com>
2012-06-05 18:13:58 +0200

    fixed typo in overrides/add_country_selectbox

--- app/overrides/add_country_selectbox.rb
+++ app/overrides/add_country_selectbox.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   :name          => "add_country_selectbox",
   :virtual_path  => "spree/layouts/spree_application",
-  :insert_before => "div#top-nav-bar",
+  :insert_before => "nav#top-nav-bar",
   :partial       => "spree/zoned/countryselect",
 )
