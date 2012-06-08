541ea4bfd53181c46e4fb94b59f1563cde9cecfd
Bernd Blume <bernd.blume@railando.com>
2012-06-08 18:50:59 +0200

    descoped helper

--- app/helpers/spree/countrylist_helper.rb
+++ app/helpers/spree/countrylist_helper.rb
@@ -1,9 +1,9 @@
-module Spree
-  BaseHelper.module_eval do
+#module Spree
+#  BaseHelper.module_eval do
     module CountrylistHelper
       def countrylist
         @zonedCountrylist ||= Country.order :name
       end
     end
-  end
-end
+#  end
+#end
