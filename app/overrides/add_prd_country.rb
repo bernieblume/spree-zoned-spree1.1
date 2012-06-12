564801420c9d9d23cc5c796d50ea2df6a9f86e59
Bernd Blume <bernd.blume@railando.com>
2012-06-12 13:48:13 +0200

    fixed Deface stuff

--- app/overrides/add_prd_country.rb
+++ app/overrides/add_prd_country.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   :name          => "add_prd_country",
   :virtual_path  => "spree/admin/products/index",
   :insert_before => "li#new_product_link",
-  :partial       => "spree/admin/zoned/countryselect",
+  :partial       => "spree/admin/zoned/prd_country",
 )
