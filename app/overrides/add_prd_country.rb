79c79e1d6f45fdde4e7d96707202c1e16204b44a
Bernd Blume <bernd.blume@railando.com>
2012-06-12 14:27:17 +0200

    changed virtual_path in override

--- app/overrides/add_prd_country.rb
+++ app/overrides/add_prd_country.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   :name          => "add_prd_country",
-  :virtual_path  => "spree/admin/products/index",
+  :virtual_path  => "spree/admin/layouts/admin",
   :insert_before => "li#new_product_link",
   :partial       => "spree/admin/zoned/prd_country",
 )
