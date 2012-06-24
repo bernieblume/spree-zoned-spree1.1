6e9d6aeb7fb0657a0297b124fc413b4daec89e44
Bernd Blume <bernd.blume@railando.com>
2012-06-24 22:05:51 +0200

    fixed js include tag

--- app/overrides/add_js_prd_index.rb
+++ app/overrides/add_js_prd_index.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   :name          => "add_js_prd_index",
   :virtual_path  => "spree/admin/products/index",
   :insert_before => "[data-hook='toolbar']",
-  :text          => "<%= javascript_include_tag 'admin/zoned' %>",
+  :partial       => "spree/admin/zoned/jstag",
 )
